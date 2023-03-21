cb54f8e32671a040f77d07a6d4849c25b3c2b434
Phillipe Jarnefors <phillipe_jarnefors@hotmail.com>
2023-03-21 14:36:25 +0100

    Boilerplate

---
+++ print_pages.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById('navbar-pages')
+const contentSec = document.getElementById('root')
+
+export default function printPages(pages) {
+	let ul = document.createElement("ul")
+	pages.map(page => {
+		console.log("page", page.title.rendered);
+		let li = document.createElement("li")
+		li.innerText = page.title.rendered;
+
+		ul.appendChild(li)
+
+		li.addEventListener("click", () => {
+			console.log("click på page", page.id);
+			let pageContent = document.createElement("div");
+			pageContent.innerHTML = page.content.rendered;
+			contentSec.appendChild(pageContent);
+		})
+	})
+	menu.appendChild(ul);
+}
--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello Hammarhaj");
+'use strict';
+
+// Här skriver du din import av modul.
+import printPages from "./print_pages.js";
+
+// Här kallar du på din funktion i din egna modul
+
+
+fetch("http://167.71.35.197/index.php/wp-json/wp/v2/pages")
+	.then(res => res.json())
+	.then(data => {
+		console.log("sidor", data);
+		printPages(data);
+	})
