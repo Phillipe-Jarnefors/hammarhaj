2c2a9640a72e7193ad63cb8c33959b3d189478c8
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-03-23 09:06:45 +0100

    Changed a classname

--- print_pages.js
+++ print_pages.js
@@ -6,14 +6,14 @@ const contentSec = document.getElementById('root')
 export default function printPages(pages) {
 	let ul = document.createElement("ul")
 	pages.map(page => {
-		console.log("page", page.title.rendered);
+		//console.log("page", page.title.rendered);
 		let li = document.createElement("li")
 		li.innerText = page.title.rendered;
 
 		ul.appendChild(li)
 
 		li.addEventListener("click", () => {
-			console.log("click på page", page.id);
+			//console.log("click på page", page.id);
 			contentSec.innerText = "";
 
 			if(li.innerText === "Front Page"){
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ import printLandingPage from "./print_landing_page.js";
 fetch("http://167.71.35.197/index.php/wp-json/wp/v2/pages")
 	.then(res => res.json())
 	.then(data => {
-		console.log("sidor", data);
+		//console.log("sidor", data);
 		printPages(data);
 		printLandingPage(data);
 	})
