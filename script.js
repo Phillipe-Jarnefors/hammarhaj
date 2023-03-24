bfc002fd5891cc01c4a13677e64c094b546d2aae
S-Gabrie1 <sergegabriel24@gmail.com>
2023-03-24 12:51:44 +0100

    Shop/Cart/localStorage Fixed

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 'use strict';
 
+//------------------------------------
 // Här skriver du din import av modul.
+//------------------------------------
 
 import printPages from "./print_pages.js";
 import printLandingPage from "./print_landing_page.js";
 import printSocialLinks from "./print_social_links.js";
 
-// Här kallar du på din funktion i din egna modul
 
+
+//------------------------
+//	CREATE STORAGE
+//------------------------
+
+if(!localStorage.getItem("cart")){
+    let cart = []
+	localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+
+//---------------------------------------------- 
+// Här kallar du på din funktion i din egna modul
+//----------------------------------------------,
 
 fetch("http://167.71.35.197/index.php/wp-json/wp/v2/pages")
 	.then(res => res.json())
